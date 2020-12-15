995a1cade3a73d1505d70a0b2bcdabee6734ac10
Ch4nYH <hfercxx@hotmail.com>
2020-12-15 20:54:14 +0800

    update

---
+++ main_2.py
@@ -0,0 +1,245 @@
+# Importing Libraries
+import argparse
+import copy
+import os
+from pickle import dump
+import sys
+import numpy as np
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import os
+from tensorboardX import SummaryWriter
+import torchvision.utils as vutils
+import seaborn as sns
+import torch.nn.init as init
+import pickle
+
+import torch.nn.utils.prune as prune
+# Custom Libraries
+import utils
+
+# Tensorboard initialization
+writer = SummaryWriter()
+
+# Plotting Style
+sns.set_style('darkgrid')
+
+# Main
+def main(args, ITE=0):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Data Loader
+    transform=transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.1307,), (0.3081,))])
+    if args.dataset == "mnist":
+        traindataset = datasets.MNIST('../data', train=True, download=True,transform=transform)
+        testdataset = datasets.MNIST('../data', train=False, transform=transform)
+        from archs.mnist import AlexNet, LeNet5, fc3, fc5, vgg, resnet
+
+    elif args.dataset == "cifar10":
+        traindataset = datasets.CIFAR10('../data', train=True, download=True,transform=transform)
+        testdataset = datasets.CIFAR10('../data', train=False, transform=transform)      
+        from archs.cifar10 import AlexNet, LeNet5, fc1, vgg, resnet, densenet 
+
+    elif args.dataset == "fashionmnist":
+        traindataset = datasets.FashionMNIST('../data', train=True, download=True,transform=transform)
+        testdataset = datasets.FashionMNIST('../data', train=False, transform=transform)
+        from archs.mnist import AlexNet, LeNet5, fc1, vgg, resnet 
+
+    elif args.dataset == "cifar100":
+        traindataset = datasets.CIFAR100('../data', train=True, download=True,transform=transform)
+        testdataset = datasets.CIFAR100('../data', train=False, transform=transform)   
+        from archs.cifar100 import AlexNet, fc1, LeNet5, vgg, resnet  
+
+    else:
+        print("\nWrong Dataset choice \n")
+        exit()
+
+    train_loader = torch.utils.data.DataLoader(traindataset, batch_size=args.batch_size, shuffle=True, num_workers=0,drop_last=False)
+    #train_loader = cycle(train_loader)
+    test_loader = torch.utils.data.DataLoader(testdataset, batch_size=args.batch_size, shuffle=False, num_workers=0,drop_last=True)
+    
+    model = fc5.fc5().to(device)
+    model.apply(weight_init)
+
+    # Copying and Saving Initial State
+    output_dir = args.output_dir if args.output_dir else f"{os.getcwd()}/saves/{args.arch_type}/{args.dataset}/"
+    
+    initial_state_dict = copy.deepcopy(model.state_dict())
+    utils.checkdir(output_dir)
+    torch.save(initial_state_dict, os.path.join(output_dir, f"initial_state_dict_{args.prune_type}.pth.tar"))
+    optimizer = torch.optim.Adam(model.parameters(), weight_decay=args.weight_decay)
+    criterion = nn.CrossEntropyLoss()
+
+
+    # Pruning
+    # NOTE First Pruning Iteration is of No Compression
+    
+
+    dump_dir = args.dump_dir if args.dump_dir else f"{os.getcwd()}/dumps/lt/{args.arch_type}/{args.dataset}/"
+
+    for _ite in range(args.start_iter, ITERATION):
+        best_accuracy = 0
+        if not _ite == 0:
+            utils.pruning_generate(model, 0.2, method=args.pruning_method)
+            model_state_dict = model.state_dict()
+            model_orig_weight = utils.rewind_weight(initial_state_dict, model_state_dict.keys())
+            model_state_dict.update(model_orig_weight)
+            model.load_state_dict(model_state_dict)
+            torch.save(model.state_dict(), os.path.join(output_dir, f"{_ite}_model_init_{args.prune_type}.pth.tar"))
+            optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+        pbar = tqdm(range(args.end_iter))
+        for iter_ in pbar:
+            if iter_ % args.valid_freq == 0:
+                print(model.classifier[8].weight_orig)
+                accuracy = test(model, test_loader, criterion)
+                if accuracy > best_accuracy:
+                    best_accuracy = accuracy
+                    utils.checkdir(output_dir)
+                    torch.save(model.state_dict(),os.path.join(output_dir, f"{_ite}_model_{args.prune_type}.pth.tar"))
+            # Training
+            if not args.pruning_method == 'random':
+                loss = train(model, train_loader, optimizer, criterion)
+            else:
+                loss = 100
+        
+            if iter_ % args.print_freq == 0:
+                pbar.set_description(
+                    f'Train Epoch: {iter_}/{args.end_iter} Loss: {loss:.6f} Accuracy: {accuracy:.2f}% Best Accuracy: {best_accuracy:.2f}%')       
+
+# Function for Training
+def train(model, train_loader, optimizer, criterion):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.train()
+    for batch_idx, (imgs, targets) in enumerate(train_loader):
+        optimizer.zero_grad()
+        imgs, targets = imgs.to(device), targets.to(device)
+        output = model(imgs)
+        train_loss = criterion(output, targets)
+        train_loss.backward()
+        optimizer.step()
+    return train_loss.item()
+
+# Function for Testing
+def test(model, test_loader, criterion):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
+            correct += pred.eq(target.data.view_as(pred)).sum().item()
+        test_loss /= len(test_loader.dataset)
+        accuracy = 100. * correct / len(test_loader.dataset)
+    return accuracy
+
+# Function for Initialization
+def weight_init(m):
+    '''
+    Usage:
+        model = Model()
+        model.apply(weight_init)
+    '''
+    if isinstance(m, nn.Conv1d):
+        init.normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.Conv2d):
+        init.xavier_normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.Conv3d):
+        init.xavier_normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.ConvTranspose1d):
+        init.normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.ConvTranspose2d):
+        init.xavier_normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.ConvTranspose3d):
+        init.xavier_normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.BatchNorm1d):
+        init.normal_(m.weight.data, mean=1, std=0.02)
+        init.constant_(m.bias.data, 0)
+    elif isinstance(m, nn.BatchNorm2d):
+        init.normal_(m.weight.data, mean=1, std=0.02)
+        init.constant_(m.bias.data, 0)
+    elif isinstance(m, nn.BatchNorm3d):
+        init.normal_(m.weight.data, mean=1, std=0.02)
+        init.constant_(m.bias.data, 0)
+    elif isinstance(m, nn.Linear):
+        init.xavier_normal_(m.weight.data)
+        init.normal_(m.bias.data)
+    elif isinstance(m, nn.LSTM):
+        for param in m.parameters():
+            if len(param.shape) >= 2:
+                init.orthogonal_(param.data)
+            else:
+                init.normal_(param.data)
+    elif isinstance(m, nn.LSTMCell):
+        for param in m.parameters():
+            if len(param.shape) >= 2:
+                init.orthogonal_(param.data)
+            else:
+                init.normal_(param.data)
+    elif isinstance(m, nn.GRU):
+        for param in m.parameters():
+            if len(param.shape) >= 2:
+                init.orthogonal_(param.data)
+            else:
+                init.normal_(param.data)
+    elif isinstance(m, nn.GRUCell):
+        for param in m.parameters():
+            if len(param.shape) >= 2:
+                init.orthogonal_(param.data)
+            else:
+                init.normal_(param.data)
+
+
+if __name__=="__main__":
+    
+    #from gooey import Gooey
+    #@Gooey      
+    
+    # Arguement Parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--lr",default= 1.2e-3, type=float, help="Learning rate")
+    parser.add_argument("--weight_decay",default=1e-4, type=float, help="Weight decay")
+    parser.add_argument("--batch_size", default=512, type=int)
+    parser.add_argument("--start_iter", default=0, type=int)
+    parser.add_argument("--end_iter", default=100, type=int)
+    parser.add_argument("--print_freq", default=1, type=int)
+    parser.add_argument("--valid_freq", default=1, type=int)
+    parser.add_argument("--resume", action="store_true")
+    parser.add_argument("--prune_type", default="lt", type=str, help="lt | reinit")
+    parser.add_argument("--gpu", default="0", type=str)
+    parser.add_argument("--dataset", default="mnist", type=str, help="mnist | cifar10 | fashionmnist | cifar100")
+    parser.add_argument("--arch_type", default="fc1", type=str, help="fc1 | lenet5 | alexnet | vgg16 | resnet18 | densenet121")
+    parser.add_argument("--prune_percent", default=10, type=int, help="Pruning percent")
+    parser.add_argument("--prune_iterations", default=35, type=int, help="Pruning iterations count")
+    parser.add_argument('--initial_state_dict', type=str, default=None, help='Start from a specified state_dict')
+    parser.add_argument('--output_dir', type=str, default=None, help='output dir')
+    parser.add_argument('--plot_dir', type=str, default=None, help='plot dir')
+    parser.add_argument('--dump_dir', type=str, default=None, help='dump dir')
+    parser.add_argument('--pruning-method', type=str, default='l1')
+    args = parser.parse_args()
+ 
+    os.environ["CUDA_VISIBLE_DEVICES"]=args.gpu
+    main(args, ITE=1)
